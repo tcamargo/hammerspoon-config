2aebe89906274597818928eb7c2ba306ff5611dd
Tiago Camargo <tcamargo@gmail.com>
2016-02-17 21:21:23 -0200

    Simplify config reload

--- init.lua
+++ init.lua
@@ -18,15 +18,5 @@ wifiWatcher = hs.wifi.watcher.new(muteSpeakersAtWorkCallback)
 wifiWatcher:start()
 
 -- Fancy configuration reloading
-function reloadConfig(files)
-  doReload = false
-  for _,file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
-  if doReload then
-    hs.reload()
-  end
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.pathwatcher.new(hs.configdir, hs.reload):start()
+hs.alert.show("Config Reloaded")
