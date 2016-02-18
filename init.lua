a3f7a34e1cc276eccd4321893732713fc48e9d3f
Tiago Camargo <tcamargo@gmail.com>
2016-02-17 22:13:42 -0200

    Enable proxy at work

--- init.lua
+++ init.lua
@@ -1,20 +1,33 @@
 local log = hs.logger.new('discworld','debug')
 
-local wifiWatcher = nil
-local SAPSSID = "SAP-Corporate"
+local workSSID = "SAP-Corporate"
+local proxyLocation = {"Apple", "Location", "SAP"}
+local noProxyLocation = {"Apple", "Location", "No proxy"}
 
-function muteSpeakersAtWorkCallback()
-  newSSID = hs.wifi.currentNetwork()
+-- Location based settings
+-- @work: mute speakers, enable proxy
+-- elsewhere: disable proxy
+function atWorkCallback()
+  local newSSID = hs.wifi.currentNetwork()
+  local finder = hs.appfinder.appFromName("Finder")
 
-  if newSSID == SAPSSID then
-    log.d("Setting volume to 0")
+  -- do nothing on disconnections
+  if newSSID == nil then
+    return
+  end
+
+  if newSSID == workSSID then
+    -- @work settings
+    hs.alert("@work")
     hs.audiodevice.defaultOutputDevice():setVolume(0)
+    finder:selectMenuItem(proxyLocation)
+    -- hs.alert("Proxy enabled")
   else
-    log.d("Setting volume to 25")
-    hs.audiodevice.defaultOutputDevice():setVolume(25)
+    finder:selectMenuItem(noProxyLocation)
+    -- hs.alert("Proxy disabled")
   end
 end
-wifiWatcher = hs.wifi.watcher.new(muteSpeakersAtWorkCallback)
+wifiWatcher = hs.wifi.watcher.new(atWorkCallback)
 wifiWatcher:start()
 
 -- ctrl+alt+del :)
